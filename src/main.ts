49d3eac7a1a86fb834fab2fecfcb547f4b6be98e
PauloMisaki <ph.sorata@gmail.com>
2021-06-06 16:31:52 -0300

    updates readme and main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  });
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
